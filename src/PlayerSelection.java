a20fca1c35e349c86d99aae2b7a6fa0c91a4b136
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2025-09-21 23:57:33 +0000

    Starting the work.

--- src/PlayerSelection.java
+++ src/PlayerSelection.java
@@ -1,2 +1,6 @@
+//Date; 19/09/2025
+//Author: Harriet Yayra Boven Fiahagbe
+//Description: To Create a short Java Program that helps coach to make decisions about a player based on certain factors and statistics
+
 public class PlayerSelection {
 }
