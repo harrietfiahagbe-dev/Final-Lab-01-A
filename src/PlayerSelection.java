c9ba44c557996024a5dfbf2318e3316b50da75cf
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2025-09-22 00:14:23 +0000

    Starting the work now.

--- src/PlayerSelection.java
+++ src/PlayerSelection.java
@@ -2,5 +2,17 @@
 //Author: Harriet Yayra Boven Fiahagbe
 //Description: To Create a short Java Program that helps coach to make decisions about a player based on certain factors and statistics
 
+
+//Importing Scanner Package to allow input
+import java.util.Scanner;
+
+//Declaring the main function, to allow the program to run
 public class PlayerSelection {
+    public static void main(String[] args) {
+        //Creating Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+        //Declaring variable to use in the program
+
+    }
 }
